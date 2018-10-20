46cba5cbc231b2da1a0133ba1a017b74d0ca8684
Kaisinel <almantusk@gmail.com>
2018-10-20 23:10:39 +0300

    Lesson 1: class and scope

--- OOP lesson series/Program.cs
+++ OOP lesson series/Program.cs
@@ -1,21 +1,60 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+/* _____________________________________Storm312_____________________________________________
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * ~~~~~~~~ William M-W ~~~~~~~~
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * 
+ */
 
+// Including scope of a namespace
+
+using System;
+using System.Runtime.CompilerServices;
+using OOP_lesson_series.Lesson_One._Classes_and_scope;
+
+// Main program namespace
 namespace OOP_lesson_series
 {
+    // Holder of starting point of a program
     class Program
     {
+        // Starting point of a program
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Baby baby1 = null;
+            Baby baby2 = null;
+            Baby baby3 = null;
+            Baby baby4 = null;
+            Baby baby5 = null;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            baby1 = new Baby();
+            baby2 = new Baby("Michael");
+            baby3 = TryCreateBaby("Ronny", new DateTime(2018, 5, 4));
+            baby4 = TryCreateBaby("Questionable", new DateTime(2019, 12, 12));
+            baby5 = TryCreateBaby("Questionable", new DateTime(2006, 12, 12));
+
+
+            Baby[] babies = { baby1, baby2, baby3, baby4 };
+            foreach (Baby baby in babies)
+            {
+                if(baby != null)
+                    Console.WriteLine(baby);
+            }
+        }
+
+        private static Baby TryCreateBaby(string name, DateTime date)
+        {
+            Baby baby = null;
+            try
+            {
+                baby = new Baby(name, date);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Invalid baby:{e.Message}");
+            }
+
+            return baby;
         }
     }
 }
